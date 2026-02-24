3611f464116301d9338af483c67c07f2bc29ae34
bsdestroyer29-sudo <bsdestroyer29@gmail.com>
2026-02-24 19:22:22 +0200

    Update DMSession.js

--- models/DMSession.js
+++ models/DMSession.js
@@ -2,9 +2,13 @@ import mongoose from "mongoose";
 
 const DMSessionSchema = new mongoose.Schema({
   guildId: { type: String, required: true },
-  userId: { type: String, required: true },
+  userId: { type: String, required: true, unique: true },
+
   staffId: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now }
-});
+
+  logChannelId: { type: String, required: true },
+
+  active: { type: Boolean, default: true }
+}, { timestamps: true });
 
 export default mongoose.model("DMSession", DMSessionSchema);
