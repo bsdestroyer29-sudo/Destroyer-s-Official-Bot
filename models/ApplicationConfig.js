c04635f58377be061bdd18e94666b180ad3d92cc
bsdestroyer29-sudo <bsdestroyer29@gmail.com>
2026-02-24 20:43:56 +0200

    Update ApplicationConfig.js

--- models/ApplicationConfig.js
+++ models/ApplicationConfig.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 const ApplicationConfigSchema = new mongoose.Schema({
   guildId: { type: String, required: true },
   panelChannelId: { type: String, required: true },
-  reviewChannelId: { type: String, required: true },
   title: { type: String, required: true },
   description: { type: String, required: true },
   questions: [{ type: String }]
