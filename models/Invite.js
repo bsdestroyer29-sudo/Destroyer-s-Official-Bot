c82f7c92f053782df1b35132defd9c3f407aeb5c
bsdestroyer29-sudo <bsdestroyer29@gmail.com>
2026-02-24 20:24:32 +0200

    Update Invite.js

--- models/Invite.js
+++ models/Invite.js
@@ -2,9 +2,13 @@ import mongoose from "mongoose";
 
 const InviteSchema = new mongoose.Schema({
   guildId: { type: String, required: true },
-  code: { type: String, required: true },
   inviterId: { type: String, required: true },
-  uses: { type: Number, default: 0 }
+
+  invites: { type: Number, default: 0 },
+  fakeInvites: { type: Number, default: 0 },
+  leaves: { type: Number, default: 0 },
+
+  invitedUsers: [{ type: String }]
 });
 
 export default mongoose.model("Invite", InviteSchema);
