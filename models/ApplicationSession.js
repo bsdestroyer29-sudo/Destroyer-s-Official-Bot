79aef02280a7f8fb26a0a8ceb3b12644ddf6fe1d
bsdestroyer29-sudo <bsdestroyer29@gmail.com>
2026-02-25 21:35:36 +0200

    Update ApplicationSession.js

--- models/ApplicationSession.js
+++ models/ApplicationSession.js
@@ -3,9 +3,18 @@ import mongoose from "mongoose";
 const ApplicationSessionSchema = new mongoose.Schema({
   guildId: String,
   userId: String,
-  answers: [{ question: String, answer: String }],
+  panelMessageId: String,
+
   currentQuestion: { type: Number, default: 0 },
+  answers: [
+    {
+      question: String,
+      answer: String
+    }
+  ],
+
   completed: { type: Boolean, default: false }
+
 }, { timestamps: true });
 
 export default mongoose.model("ApplicationSession", ApplicationSessionSchema);
