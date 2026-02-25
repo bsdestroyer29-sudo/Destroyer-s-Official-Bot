42a65d0342828214157f0d8d4227c5501e306ae8
bsdestroyer29-sudo <bsdestroyer29@gmail.com>
2026-02-25 21:34:58 +0200

    Update ApplicationConfig.js

--- models/ApplicationConfig.js
+++ models/ApplicationConfig.js
@@ -1,12 +1,17 @@
 import mongoose from "mongoose";
 
 const ApplicationConfigSchema = new mongoose.Schema({
-  guildId: { type: String, required: true },
-  panelChannelId: { type: String, required: true },
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  questions: [{ type: String }],
+  guildId: String,
+  panelChannelId: String,
+  panelMessageId: String,
+
+  title: String,
+  description: String,
+
+  questions: [String],
+
   isOpen: { type: Boolean, default: true }
-});
+
+}, { timestamps: true });
 
 export default mongoose.model("ApplicationConfig", ApplicationConfigSchema);
